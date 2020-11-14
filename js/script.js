6fed20a2875980d1b9f03fc8ab08202ebb36feec
commaeng09 <41318974+commaeng09@users.noreply.github.com>
2020-11-14 20:23:12 +0900

    asdf

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 $(function(){
+
+    $(window).scroll(function(){
+        var height = $(document).scrollTop();
+        if(height > 0){
+            $('header').css('position','fixed');
+        }else if(height == 0){
+            $('header').css('position','relative');
+        }
+    });
+
+
     $(".nav").hover(function(){
         $('.main-nav li ul').slideToggle();
         $('header.main').toggleClass('active');
@@ -18,11 +29,70 @@ $(function(){
     });
 
 
-    $('.card-2').click(function(){
+    $('.card-2 img').click(function(){
         $(".main-popup").css('display','block');
     });
 
     $('.pop-close').click(function(){
         $(".main-popup").css('display','none');
+    });
+
+
+    // 구매물품 접수 팝업
+    $('.go-to-cart').click(function(){
+        $('.buy-receive').css('display','block');
+    });
+    $('.buy-btn.cancle').click(function(){
+        $('.buy-receive').css('display','none');
+    });
+
+    // 상태 관리 팝업
+    $('.open-stat1').click(function(){
+        $('.stat-pop').css('display','block');
+    });
+    $(".stat-btn .close").click(function(){
+        $('.stat-pop').css('display','none');
+    });
+
+    // 댓글 달기 팝업
+    $(".openrq").click(function(){
+        $(".request").css('display','block');
+    });
+    $(".request .cancle").click(function(){
+        $('.request').css('display','none');
+    });
+
+    // 캠페인 팝업
+    $('.write-btn-2 a').click(function(){
+        $('.cp-popup').css('display','block');
+    });
+    $('.camp-btn .cancle').click(function(){
+        $('.cp-popup').css('display','none');
+    });
+
+
+    var fileTarget = $('.buy-attach-wrap .upload-hidden');
+
+    fileTarget.on('change', function(){
+        if(window.FileReader){
+            var filename = $(this)[0].files[0].name;
+        } else {
+            var filename = $(this).val().split('/').pop().split('\\').pop();
+        }
+
+        $(this).siblings('.upload-name').val(filename);
+    });
+
+
+    var campTarget =  $('.camp-hidden');
+
+    campTarget.on('change', function(){
+        if(window.FileReader){
+            var campfile = $(this)[0].files[0].name;
+        } else {
+            var campfile = $(this).val().split('/').pop().split('\\').pop();
+        }
+
+        $(this).siblings('.imageUpload').val(campfile);
     });
 });
